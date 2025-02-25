828625040a67b18d552df94d644ef99897f1e46c
nikadavitaaa <davitashvilinikoloz25@gmail.com>
2025-02-25 17:53:56 +0400

    second commit

---
+++ GROUP 64/Level 01/homework/task1.py
@@ -0,0 +1,20 @@
+name = "nika" # ეს არის str (string) ტიპის ცვლადი, 
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშნელობის მიმნიჭებელი სიმბოლო
+# "nika" არის ცვლადისთვის მნიშვნელობა
+surname ="davitashvili"
+age = 15 # ეს არის int(integer) მთელი რიცხვი
+
+
+height = 175.1 # ეს არის float ტიპის ცვლადი( ათწილადი )
+#boolean ( bool ) ტიპის ცვლადი
+
+
+knows_proggraming = True  #True ან False
+
+is_ugly = False  #snakecase ( უბრალოდ წერის სტილი სტანდარტულად )
+
+isUgly = False  #javascript ( camelcase )
+
+
+print(name + " " + surname + " " +str(age) + " " + str(height))
